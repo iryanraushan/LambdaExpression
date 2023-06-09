8bca6db8b10d397ac63ff5f8b0563ce8bfe0794b
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-06-09 19:25:55 +0530

    Predicate Functional Interface

---
+++ src/learnDemo/LE.java
@@ -0,0 +1,16 @@
+package learnDemo;
+@FunctionalInterface
+interface Cab {
+
+    public void bookCab();
+    
+}   
+
+public class LE {
+    public static void main(String[] args) {
+
+        Cab cab = () -> System.out.println("cab is booked...");
+
+        cab.bookCab();
+    }
+}
